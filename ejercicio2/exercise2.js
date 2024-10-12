60fcf7b40ed7e3a8c7965d5e56a3d3f0e7d98a0e
Héctor Rodríguez Planelles <hectorrodriguezplanelles@gmail.com>
2024-10-12 16:59:32 +0200

    Final Ejercicio2

--- ejercicio2/exercise2.js
+++ ejercicio2/exercise2.js
@@ -17,59 +17,155 @@ newProdForm.image.addEventListener('change', event => {
 
 newProdForm.addEventListener('submit',(e) =>{
     e.preventDefault()
-
+    // nombrar varibles
+    const arrayFechas = {
+        0 : "Dom",
+        1 : "Lun",
+        2 : "Mar",
+        3 : "Mie",
+        4 : "Jue",
+        5 : "Vie",
+        6 : "Sab"
+    }
+    let arrayDias = []
     const container = document.getElementById("placesContainer")
     const name = newProdForm.elements.namedItem("name")
     const description = newProdForm.elements.namedItem("description")
-    const cuisine = newProdForm.elements.namedItem("cuisene")
+    const cuisine = newProdForm.elements.namedItem("cuisine")
     const days = Array.from(newProdForm.elements.namedItem("days"))
                         .filter((input) => input.checked)
-                        .map((input) => input.value)
+                        .map((input)=>input.value)
     const phone = newProdForm.elements.namedItem("phone")
-    const img = valorImage
-    //create div 1
-    // let div = document.createElement("div")
-    // div.classList.add("col")
-    //create div 2
-    // let div2 = document.createElement("div")
-    // div2.classList.add("card", "h-100", "shadow")
-    // div.append(div2)
-    // obtain image
-    // let image = obtainImage(img)
-    //append image to div2
-    // div2.append(image)
-    //create elemet div 3
-    // let div3 = document.createElement("div")
-    // div3.classList.add("card-body")
-    //append div3 to div2
-    // div2.append(div3)
-    //create elemet h4
-    // let h4 = obtainH4(name)
-    //append h4 to div3
-    // div3.append(h4)
-
-    // appent to container
-    //container.append(div)
-
+    let resultado,resultado2,resultado3,resultado4,resultado5,resultado6=false
+    resultado= comprobarName()
+    resultado2 = comprobarDescription()
+    resultado3 = comprobarCocina()
+    resultado4 = comprobarDays()
+    resultado5 = comprobarPhone()
+    resultado6 = ComprobarImg()
+    // construccion del template
     const userHTML = userTemplate.content.cloneNode(true);
     const div  = userHTML.firstElementChild;
     div.querySelector("img").src = valorImage;
     div.querySelector("h4").textContent = name.value;
     div.querySelector("p").textContent = description.value;
-    container.append(div)
+    const [daysElement, phoneElement, cocinaElement] = div.querySelectorAll("small")
+    days.forEach(d=>{
+        arrayDias.push(arrayFechas[d])
+    })
+    daysElement.append(arrayDias)
+    phoneElement.append(phone.value)
+    cocinaElement.append(cuisine.value)
+    const [abierto,cerrado] = div.querySelectorAll("span")
+    dayOpenClose(abierto,cerrado,days)
+    
 
+    if(resultado && resultado2 && resultado3 && resultado4 && resultado5 && resultado6){
+        container.append(div)
+        newProdForm.reset()
+        imgPreview.classList.add("d-none")
+        limpiarClases()
+
+    }
+    
 })
-//create element image
-// function obtainImage(img){
-//     let image =  document.createElement("img")
-//     image.classList.add("card-img-top")
-//     image.src = img
-//     return image
-// }
-// create element h4
-// function obtainH4(name){
-//     let nombre = document.createElement("h4")
-//     nombre.classList.add("card-title")
-//     nombre.textContent=name.value
-//     return nombre
-// }
+function limpiarClases(){
+    const name = document.getElementById("name")
+    const description = document.getElementById("description")
+    const cuisine = document.getElementById("cuisine")
+    const phone = document.getElementById("phone")
+    const img= document.querySelector("#image")
+    name.classList.remove("is-valid")
+    name.classList.remove("is-invalid")
+    description.classList.remove("is-invalid")
+    description.classList.remove("is-valid")
+    cuisine.classList.remove("is-invalid")
+    cuisine.classList.remove("is-valid")
+    phone.classList.remove("is-valid")
+    phone.classList.remove("is-invalid")
+    img.classList.add("d-none")
+}
+
+function dayOpenClose(abierto,cerrado,days){
+    const date = new Date()
+    if(days.includes(date.getDay().toString())){
+        abierto.classList.remove("d-none")
+        cerrado.classList.add("d-none")
+    }else{
+        abierto.classList.add("d-none")
+        cerrado.classList.remove("d-none")
+    }
+}
+
+function comprobarName(){
+    const name = document.getElementById("name")
+    if(name.value.match(/^[a-z\s]+/)){
+        name.classList.add("is-valid")
+        name.classList.remove("is-invalid")
+        return true
+    }else{
+        name.classList.add("is-invalid")
+        name.classList.remove("is-valid")
+    }
+
+}
+function comprobarDescription(){
+    const description = document.getElementById("description")
+    if(description.value.match(/^\w+/)){
+        description.classList.add("is-valid")
+        description.classList.remove("is-invalid")
+        return true
+    }else{
+        description.classList.add("is-invalid")
+        description.classList.remove("is-valid")
+    }
+}
+function comprobarCocina(){
+    const cuisine = document.getElementById("cuisine")
+    if(cuisine.value.match(/^\w+/)){
+        cuisine.classList.add("is-valid")
+        cuisine.classList.remove("is-invalid")
+        return true
+    }else{
+        cuisine.classList.add("is-invalid")
+        cuisine.classList.remove("is-valid")
+    }
+}
+function comprobarDays(){
+    const days = Array.from(newProdForm.elements.namedItem("days"))
+        .filter((input) => input.checked)
+    let daysError = document.getElementById("daysError")
+
+        if(days.length>0){
+            daysError.classList.add("d-none")
+            return true
+        }else{
+            daysError.classList.remove("d-none")
+            return false
+        }
+
+}
+
+function comprobarPhone(){
+    const phone = document.getElementById("phone")
+    if(phone.value.match(/^[0-9]{9}/)){
+        phone.classList.add("is-valid")
+        phone.classList.remove("is-invalid")
+        return true
+    }else{
+        phone.classList.add("is-invalid")
+        phone.classList.remove("is-valid")
+    }
+}
+function ComprobarImg(){
+    const img = document.getElementById("image")
+    if(img.value){
+        img.classList.add("is-valid")
+        img.classList.remove("is-invalid")
+        return true
+    }else{
+        img.classList.add("is-invalid")
+        img.classList.remove("is-valid")
+    }
+}
+
