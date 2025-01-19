b8ae9f35a11460a2394b276a1b36daad81259476
Héctor Rodríguez Planelles <hectorrodriguezplanelles@gmail.com>
2025-01-19 19:33:02 +0100

    Corrigiendo estructura de archivos

---
+++ foodScorePruebasç/src/classes/restaurant-service.ts
@@ -0,0 +1,23 @@
+import { SERVER } from "../constants"
+import { RestaurantsResponse, SingleRestaurantResponse } from "../interfaces/responses"
+import { Restaurant, RestaurantInsert } from "../interfaces/restaurant"
+import { Http } from "./http"
+
+export class RestaurantService {
+    #http
+    constructor(){
+        this.#http = new Http()
+    }
+
+    async getAll(): Promise<Restaurant[]>{
+        const resp = await this.#http.get<RestaurantsResponse>(SERVER+"/restaurants")
+        return resp.restaurants;
+    }
+    async post(restaurants : RestaurantInsert) : Promise<RestaurantInsert>{
+        const resp = await this.#http.post<SingleRestaurantResponse,RestaurantInsert>(SERVER+"/restaurants",restaurants)
+        return resp.restaurant
+    }
+    delete(id:number){
+        this.#http.delete<void>(SERVER+`/restaurants/${id}`)
+    }
+}
